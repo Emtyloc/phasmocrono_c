22de2da1e627e272fc8baa561fb17e8925c41d62
Emtyloc <mitoro1@miuandes.cl>
2025-11-10 00:22:45 -0300

    feat: windows out of focus keypressed.

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Timer utility for Phasmophobia ghost hunting developed in C
 
 # build windows using w64devkit console
-- gcc -o phasmocrono.exe main.c  -I include -L lib -lraylib -lgdi32 -lwinmm
+- gcc -o phasmocrono.exe main.c -mwindows -I include -L lib -lraylib -lgdi32 -lwinmm
--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include "raylib.h"
 
+#ifdef _WIN32
+    #include <windows.h>  //GetAsyncKeyState()
+#endif
+
 typedef struct {
     int minutes;
     float seconds;
@@ -15,37 +19,35 @@ void reset(Time *time, bool *running)
 {
     time->minutes = 0;
     time->seconds = 0.0f;
-
     *running = false;
 }
 
 int main(int argc, char **argv)
 {
     SetConfigFlags(FLAG_WINDOW_UNDECORATED | FLAG_WINDOW_TRANSPARENT);
-    
+
     const int DEFAULT_WINDOW_WIDTH = 200;
     const int DEFAULT_WINDOW_HEIGHT = 75;
     const int MONITOR_ID = 0;
-    
+
     InitWindow(DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT, "Phasmocrono");
-    
+
     const char *icon_path = "./assets/icon.png";
     Image icon = LoadImage(icon_path);
     SetWindowIcon(icon);
 
     SetWindowState(FLAG_WINDOW_TOPMOST);
     SetExitKey(KEY_PAGE_UP);
-    
-    
+
     SetTargetFPS(30);
     SetWindowMonitor(MONITOR_ID);
-    
+
     int monitor_x = GetMonitorPosition(MONITOR_ID).x;
     int monitor_y = GetMonitorPosition(MONITOR_ID).y;
     int monitor_width = GetMonitorWidth(MONITOR_ID);
-    
+
     float scale = (float)monitor_width / 2560.0f;
-    
+
     int window_width = (int)(200 * scale);
     int window_height = (int)(75 * scale);
     int font_size = (int)(60 * scale);
@@ -67,9 +69,13 @@ int main(int argc, char **argv)
     while (!WindowShouldClose())
     {
         float delta_time = GetFrameTime();
-        
+
+#ifdef _WIN32
+        if (GetAsyncKeyState(VK_END) & 0x8000)      start_pause(&running);
+        if (GetAsyncKeyState(VK_NEXT) & 0x8000)     reset(&time, &running); // PAGE_DOWN = VK_NEXT
+#else
         int key_pressed = GetKeyPressed();
-        if(key_pressed)
+        if (key_pressed)
         {
             switch (key_pressed)
             {
@@ -81,33 +87,34 @@ int main(int argc, char **argv)
                     break;
             }
         }
+#endif
+
         if (running)
         {
             time.seconds += delta_time;
             if (time.seconds >= 60)
             {
-                const float rest_seconds = time.seconds - 60.0f;
-                time.seconds = rest_seconds;
+                time.seconds -= 60.0f;
                 time.minutes++;
                 if (time.minutes > 99) reset(&time, &running);
             }
         }
-        
+
         const char *time_m_text = TextFormat("%02d", time.minutes);
         const char *time_s_text = TextFormat("%05.2f", time.seconds);
-        
+
         char time_text[16] = {0};
         int position = 0;
         TextAppend(time_text, time_m_text, &position);
         TextAppend(time_text, ":", &position);
-        TextAppend(time_text, time_s_text, &position);        
-        
+        TextAppend(time_text, time_s_text, &position);
+
         Vector2 text_size = MeasureTextEx(chiller_font, time_text, font_size, font_spacing);
         Vector2 text_pos = {
             .x = (GetScreenWidth() - text_size.x) / 2,
             .y = (GetScreenHeight() - text_size.y) / 2,
         };
-        
+
         // START RENDERING
         BeginDrawing();
 
